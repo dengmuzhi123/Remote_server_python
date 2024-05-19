6b64c4f7a9fa826a44e81980f136d3dfe4aa693e
邓牧知 <139972153+dengmuzhi123@users.noreply.github.com>
2024-05-19 20:08:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import ssh
+from verification import *
+import sys
+
+#connect_mode = str(sys.argv[1])
+
+ip_address = input("输入要ping的ip地址")
+ping(ip_address)
+#print(connect_mode)
+input("Press any key to continue . . .")
